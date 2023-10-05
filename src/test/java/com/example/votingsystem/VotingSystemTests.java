aa5e34e6950d65df99800d74eca0b596d43f4db7
duycan17 <duydk.22git@vku.udn.vn>
2023-10-05 20:47:50 +0700

    rename

--- src/test/java/com/example/votingsystem/VotingSystemTests.java
+++ src/test/java/com/example/votingsystem/VotingSystemTests.java
@@ -1,10 +1,10 @@
-package com.example.prjVoteSystem;
+package com.example.votingsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PrjVoteSystemApplicationTests {
+class VotingSystemTests {
 
 	@Test
 	void contextLoads() {
